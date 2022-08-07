86f5c881736fca7512a67ddd3c0c189c5d2531a6
Mohamed Salah Hussein Sayed <75040202+mohamedsalah23@users.noreply.github.com>
2022-08-07 13:56:43 +0200

    Add files via upload

---
+++ #Numper.py
@@ -0,0 +1,47 @@
+# -------------
+# -- Numbers --
+# -------------
+
+# Integer
+
+print(type(1))
+print(type(100))
+print(type(10))
+print(type(-10))
+print(type(-110))
+
+# Float
+
+print(type(1.500))
+print(type(100.99))
+print(type(-10.99))
+print(type(0.99))
+print(type(-0.99))
+
+# Complex
+
+myComplexNumber = 5+6j
+
+print(type(myComplexNumber))
+
+print("Real Part Is: {}".format(myComplexNumber.real))
+print("Imaginary Part Is: {}".format(myComplexNumber.imag))
+# Numper comp
+MyCom = 10+5j
+print(type(MyCom))
+print("My real is = {}".format(MyCom.real))
+print("My imag is = {}".format(MyCom.imag))
+# [1] You Can Convert From Int To Float or Complex
+# [2] You Can Convert From Float To Int or Complex
+# [3] You Cannot Convert Complex To Any Type
+
+print(100)
+print(float(100))
+print(complex(100))
+
+print(10.50)
+print(int(10.50))
+print(complex(10.50))
+
+print(10+9j)
+print(int(10+9j))
